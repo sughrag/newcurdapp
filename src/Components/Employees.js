53e8801113b470fa169b271ec505ad09ec82f353
sughra <sughrak3@gmail.com>
2023-02-10 19:42:59 +0500

    curdapp(W2)

---
+++ src/Components/Employees.js
@@ -0,0 +1,18 @@
+const Employees=[
+    {
+        id:"1",
+        name:"sughra",
+        age:'23'
+    },
+    {
+        id:"2",
+        name:"urwah",
+        age:'23'
+    },
+    {
+        id:"3",
+        name:"Amna",
+        age:'23'
+    }
+]
+export default Employees;
